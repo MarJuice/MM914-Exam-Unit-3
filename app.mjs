c70916b33c1ab6d95f17e328f34a59ca9f60239d
MarJuice <mariushanss1@gmail.com>
2025-03-18 19:03:06 +0100

    Repurposed code from Escape The Earth. Successfully received the first challenge from the API

--- app.mjs
+++ app.mjs
@@ -1,1 +1,15 @@
+import fetch from "node-fetch";
 
+//#region api urls
+const API_URL = 'https://alchemy-kd0l.onrender.com/';
+const PLAYER_NAME = 'mariuha@uia.no';
+const CLUE_URL = `${API_URL}clue?player=${PLAYER_NAME}`;
+const startURL = `${API_URL}start?player=${PLAYER_NAME}`;
+const ANSWER_URL = `${API_URL}answer`;
+//endregion
+
+//#region fetch api
+const start = await fetch(startURL, { method: 'GET' });
+const startResponse = await start.json();
+console.log(startResponse);
+//endregion
