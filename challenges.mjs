591c0f38dfb8a4521512fc5f8df7c9dab287cd98
MarJuice <mariushanss1@gmail.com>
2025-03-20 20:41:58 +0100

    Decode the first challenge string '☉☿☽♂☉'

---
+++ challenges.mjs
@@ -0,0 +1,5 @@
+import { answer, decodePlanetarySymbols } from './app.mjs';
+
+// Challenge 1: Decode "☉☿☽♂☉"
+answer(decodePlanetarySymbols('☉☿☽♂☉')); // Answers: 'Gold Quicksilver Silver Iron Gold' 
+
